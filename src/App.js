dc80a7d3d196bc0bb75e92a681bd85bc20e8e6fe
Said-laasri <saidlaasri3@gmail.com>
2023-02-16 16:09:04 +0100

    Import component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./component/Navbar/NavBar";
+import Home from "./component/Home/Home";
+import Team from "./component/Team/Team";
+import Products from "./component/Products/Products";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Home />
+      <Products />
+      <Team />
+      <Footer />
+    </>
   );
 }
 
