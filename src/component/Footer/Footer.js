416d1fd9a3105aa89c7498641727639ee20b9506
Said-laasri <saidlaasri3@gmail.com>
2023-03-06 15:38:20 +0100

    Update footer list of options

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,6 +1,29 @@
 import React from "react";
 
 const Footer = () => {
+  const about = [
+    {
+      name: "About",
+      link: "#",
+    },
+    {
+      name: "Blog",
+      link: "#",
+    },
+    {
+      name: "Jobs",
+      link: "#",
+    },
+    {
+      name: "Press",
+      link: "#",
+    },
+    {
+      name: "Privacy",
+      link: "#",
+    },
+  ];
+
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
@@ -8,31 +31,16 @@ const Footer = () => {
           className="-mx-5 -my-2 flex flex-wrap justify-center"
           aria-label="Footer"
         >
-          <div className="px-5 py-2">
-            <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-              About
-            </a>
-          </div>
-          <div className="px-5 py-2">
-            <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-              Blog
-            </a>
-          </div>
-          <div className="px-5 py-2">
-            <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-              Jobs
-            </a>
-          </div>
-          <div className="px-5 py-2">
-            <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-              Press
-            </a>
-          </div>
-          <div className="px-5 py-2">
-            <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-              Privacy
-            </a>
-          </div>
+          {about.map((item) => (
+            <div key={item.name} className="px-5 py-2">
+              <a
+                href={item.link}
+                className="text-base text-gray-500 hover:text-gray-900"
+              >
+                {item.name}
+              </a>
+            </div>
+          ))}
         </nav>
       </div>
     </div>
