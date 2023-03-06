3c47ebf9f56784276632a4a9cb935c970e0d58b6
Said-laasri <saidlaasri3@gmail.com>
2023-03-06 15:39:11 +0100

    Update background image to fit subject

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,22 +1,22 @@
 import React from "react";
+import image from "../.././Assets/download.jpeg"
 
 const Home = () => {
   const links = [
-    { name: "Open roles", href: "#" },
-    { name: "Internship program", href: "#" },
+    { name: "Best Sellers", href: "#" },
+    { name: "Become seller", href: "#" },
     { name: "Our values", href: "#" },
     { name: "Meet our leadership", href: "#" },
   ];
   const stats = [
-    { name: "Offices worldwide", value: "12" },
-    { name: "Full-time colleagues", value: "300+" },
-    { name: "Hours per week", value: "40" },
-    { name: "Paid time off", value: "Unlimited" },
+    { name: "Online Shops", value: "12" },
+    { name: "Full-time Services", value: "300+" },
+    { name: "Hours per week", value: "27/7" },
   ];
   return (
     <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
       <img
-        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+        src={image}
         alt=""
         className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
       />
